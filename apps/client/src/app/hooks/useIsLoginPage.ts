801cc6aa35cc2e78371f2cbb6d8f20f546a23103
Romain Portanguen <portanguenr@gmail.com>
2024-10-25 18:56:34 +0200

    Deleting an useless custom hook

--- apps/client/src/app/hooks/useIsLoginPage.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation } from 'react-router-dom';
-import { useMemo } from 'react';
-
-export const useIsLoginPage = () => {
-  const location = useLocation();
-  return useMemo(() => location.pathname === '/login', [location.pathname]);
-};
