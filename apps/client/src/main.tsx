2c074a7fe8689cc7589ac91b9406879c92a0711b
Anas Barghoud <abarghoud@gmail.com>
2025-03-27 23:51:39 +0100

    Add redirect script to use the new domain in production

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -5,6 +5,14 @@ import { ThemeProvider } from '@pps-easy/ui/theme-provider';
 
 import App from './app/app';
 
+if (
+  window.location.hostname !== "pps-easy.fr" &&
+  window.location.hostname !== 'www.pps-easy.fr' &&
+  window.location.hostname !== 'localhost'
+) {
+  window.location.replace("https://www.pps-easy.fr" + window.location.pathname + window.location.search);
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
